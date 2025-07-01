371169a09a273f10fdaafca87cfa1075700db641
Guillermo Tantaleán <guillermo.rs2017@gmail.com>
2025-07-01 03:57:45 -0500

    feat(barista): add the getCuppingSessionById query object.

--- src/main/java/com/cafemetrix/cafelab/barista/domain/model/queries/GetCuppingSessionByIdQuery.java
+++ src/main/java/com/cafemetrix/cafelab/barista/domain/model/queries/GetCuppingSessionByIdQuery.java
@@ -1,4 +1,16 @@
 package com.cafemetrix.cafelab.barista.domain.model.queries;
 
-public record GetCuppingSessionByIdQuery() {
+/**
+ * Query to get a cupping session by its ID.
+ * @param cuppingSessionId The ID of the cupping session.
+ */
+public record GetCuppingSessionByIdQuery(Long cuppingSessionId) {
+    /**
+     * Constructor validation
+     */
+    public GetCuppingSessionByIdQuery {
+        if (cuppingSessionId == null || cuppingSessionId <= 0) {
+            throw new IllegalArgumentException("CuppingSessionId must be greater than 0 and not null");
+        }
+    }
 }
